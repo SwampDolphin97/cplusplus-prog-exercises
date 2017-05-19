7f35106b708397950e110ada27e25041c2696a05
Suyash Dayal <suyashdayal@gmail.com>
2017-05-19 11:44:30 +0530

    Add the third and the final version of the solution to question 3.2
    Since, this version is considered to be the final answer to question 3.2...
    
    Therefore, it will also close issue #9.
    
    For more info, please refer to issue #9 Update 3..

--- Programming Exercises/Chapter 3 - Tokens, Expressions and Control Structures/Question 2/src/Answer 3.cpp
+++ Programming Exercises/Chapter 3 - Tokens, Expressions and Control Structures/Question 2/src/Answer 3.cpp
@@ -29,27 +29,31 @@ int main() {
 
 	int* intArray = createVector(size);
 
-	cout << "An integer vector of " << size << "elements has been created." << endl;
+	cout << "An integer vector of " << size << " elements has been created." << endl;
 
 	// For testing purposes... Please uncomment the below section..
 	// Manual Test Section STARTS
-	cout << "Inputting values to the vector: " << endl;
+
+	/*
+	cout << endl << "Inputting values to the vector... " << endl;
 	for (int i = 0; i < size; i++) {
 
-		cout << "Enter the value of " << i + 1 << "th element: " << flush;
+		cout << "Enter the value of " << i + 1 << " th element: " << flush;
 		cin >> intArray[i];
 	}
 
-	cout << "Outputting values of the vector: " << endl;
+	cout << endl << "Outputting values of the vector... " << endl;
 	for (int i = 0; i < size; i++) {
 
 		cout << "intArray[" << i + 1 << "]: " << intArray[i] << endl;
 	}
+	*/
+
 	// Manual Test Section ENDS
 
 	deleteVector(intArray);
 
-	cout << "An integer vector has been destroyed." << endl;
+	cout << endl << "An integer vector has been destroyed." << endl;
 
 	return 0;
 }
