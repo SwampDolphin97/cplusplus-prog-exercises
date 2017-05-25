1eabe1a596e98783da6cd4476c3020a7fcadcdc8
Suyash Dayal <suyashdayal@gmail.com>
2017-05-25 23:39:02 +0530

    Add the final answer of question 3.6 and close issue #22
    For more info, refer to issue #22 Update 3.

--- Programming Exercises/Chapter 3 - Tokens, Expressions and Control Structures/Question 6/src/Answer 6.cpp
+++ Programming Exercises/Chapter 3 - Tokens, Expressions and Control Structures/Question 6/src/Answer 6.cpp
@@ -8,12 +8,16 @@
 
 using namespace std;
 
+// NOTE: This program only works on those cases where the data is already given and we know the number of elements present in each array.
+
+float calculateAverage(int runs, int innings, int notOuts);
+
 int main() {
 
 	string const playerName[] = {"Sachin", "Saurav", "Rahul"};
 	unsigned int const runs[] = {8430, 4200, 3350};
 	unsigned int const innings[] = {230, 130, 105};
-	unsigned int const nNotOut[] = {18, 9, 11};
+	unsigned int const notOuts[] = {18, 9, 11};
 	unsigned int const ARRAY_SIZE = 3;
 
 	cout << "------------------------------------------------------------------------------" << endl;
@@ -23,12 +27,18 @@ int main() {
 	for(unsigned int i = 0; i < ARRAY_SIZE; i++) {
 
 		cout << "| " << left << setw(15) << playerName[i] << "| " << setw(10) << runs[i] << "| " << setw(9) << innings[i] <<
-			    "| " << setw(15) << nNotOut[i] << "| " << right << setw(19) << "|" << endl;
+			    "| " << setw(15) << notOuts[i] << "| " << setw(18) << fixed << setprecision(2) << calculateAverage(runs[i], innings[i], notOuts[i]) <<
+				"|" << endl;
 	}
 
 	cout << "------------------------------------------------------------------------------" << endl;
 
 	return 0;
+}
+
+float calculateAverage(int runs, int innings, int notOuts) {
+
+	return runs / (float)(innings - notOuts);
 }
 
 
