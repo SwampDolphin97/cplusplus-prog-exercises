a19b83ef9093a320f7127750d8e4400ce30fe100
Suyash Dayal <suyashdayal@gmail.com>
2017-05-25 07:15:38 +0530

    Add on to the basic framework of answer to question 3.4
    For more info, refer to issue #11 Update 3

--- Programming Exercises/Chapter 3 - Tokens, Expressions and Control Structures/Question 4/src/Answer 4.cpp
+++ Programming Exercises/Chapter 3 - Tokens, Expressions and Control Structures/Question 4/src/Answer 4.cpp
@@ -9,6 +9,14 @@ using namespace std;
 
 int main() {
 
+	unsigned short int const principal[] = {1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000};
+	float const interestRate[] = {0.10, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.20};
+	unsigned short int const nYears[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+	cout << "-------------------------------------------------------------------------------" << endl;
+	cout << "Principal(P)" << "\t" << "|Interest Rate(r)" << "\t" << "|Number of Years(n)" << "\t" << "|Value(V)" << endl;
+	cout << "-------------------------------------------------------------------------------" << endl;
+
 	return 0;
 }
 
