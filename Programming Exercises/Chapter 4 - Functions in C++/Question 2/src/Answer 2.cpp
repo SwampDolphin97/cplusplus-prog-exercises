3198d18599317d0b720be89e4c7ed3dc0e7422ea
Suyash Dayal <suyashdayal@gmail.com>
2017-05-27 21:47:27 +0530

    Add the initial template of the second question of chapter 4

---
+++ Programming Exercises/Chapter 4 - Functions in C++/Question 2/src/Answer 2.cpp
@@ -0,0 +1,22 @@
+/*
+ * Answer 2.cpp
+ * Author: suyashd95
+ */
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+	int rows, cols;
+
+	cout << "Enter the rows of the matrix: " << flush;
+	cin >> rows;
+	cout << "Enter the columns of the matrix: " << flush;
+	cin >> cols;
+
+	return 0;
+}
+
