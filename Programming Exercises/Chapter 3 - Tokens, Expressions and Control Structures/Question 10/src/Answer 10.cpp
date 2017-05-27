9cbf9de7cb00529f36751ffac4fabfbb0d83c7f2
Suyash Dayal <suyashdayal@gmail.com>
2017-05-27 16:27:37 +0530

    Answer to question 3.10 is complete and close issue #26
    For more info, please refer to issue #26 Update 4.

--- Programming Exercises/Chapter 3 - Tokens, Expressions and Control Structures/Question 10/src/Answer 10.cpp
+++ Programming Exercises/Chapter 3 - Tokens, Expressions and Control Structures/Question 10/src/Answer 10.cpp
@@ -29,9 +29,11 @@ int main() {
 
 	for(int i = 0; i < nDomesticUsers; i++) {
 
-		cout << "|" << setw(39) << "|" << flush;
-		cout << setw(39) << "|" << endl;
+		cout << "|" << setfill(' ') << setw(23) << consumerNames[i] << setw(16) << "|" << flush;
+		cout << setw(25) << fixed << setprecision(2) << calculateBill(unitsConsumed[i]) << setw(14) << "|" << endl;
 	}
+
+	cout << "|" << setw(78) << setfill('-') << "|" << endl;
 
 	delete [] consumerNames;
 	delete [] unitsConsumed;
@@ -69,6 +71,48 @@ void populateData(string* consumerNames, unsigned int* unitsConsumed, int const 
 	cout << "Data of all the " << arraySize << " consumers have been successfully entered." << endl;
 }
 
-float calculateBill(unsigned int unitsConsumed) { return 0.0; }
+float calculateBill(unsigned int const unitsConsumed) {
+
+	float totalBill = 0.00;
+
+	float const BASE_CHARGE = 50.00;
+	float const COST_UPTO_FIRST_100_UNITS = 0.60;
+	float const COST_ABOVE_100_AND_UPTO_300_UNITS = 0.80;
+	float const COST_BEYOND_300_UNITS = 0.90;
+	float const ADDITIONAL_SURCHAGE = 0.15;
+
+	int nUnitsUpto100 = 0;
+	int nUnitsAbove100AndUpto300 = 0;
+	int nUnitsBeyond300 = 0;
+
+	totalBill += BASE_CHARGE;
+
+	if(unitsConsumed > 300) {
+
+		nUnitsUpto100 = 100;
+		nUnitsAbove100AndUpto300 = 200;
+		nUnitsBeyond300 = unitsConsumed - 300;
+
+		totalBill += ((nUnitsUpto100 * COST_UPTO_FIRST_100_UNITS) + (nUnitsAbove100AndUpto300 * COST_ABOVE_100_AND_UPTO_300_UNITS)
+				    + (nUnitsBeyond300 * COST_BEYOND_300_UNITS));
+	}
+	else if(unitsConsumed > 100 && unitsConsumed <= 300) {
+
+		nUnitsUpto100 = 100;
+		nUnitsAbove100AndUpto300 = unitsConsumed - 100;
+
+		totalBill += ((nUnitsUpto100 * COST_UPTO_FIRST_100_UNITS) + (nUnitsAbove100AndUpto300 * COST_ABOVE_100_AND_UPTO_300_UNITS));
+	}
+	else {
+
+		nUnitsUpto100 = unitsConsumed;
+		totalBill += (nUnitsUpto100 * COST_UPTO_FIRST_100_UNITS);
+	}
+
+	if(totalBill > 300.00)
+		totalBill += (totalBill * ADDITIONAL_SURCHAGE);
+
+	return totalBill;
+}
 
 
