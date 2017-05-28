cdc382ead02240ecf4cb93a22ce74eccba84a796
Suyash Dayal <suyashdayal@gmail.com>
2017-05-28 06:38:50 +0530

    Add the completed sections of the answer to question 3.8
    For more info, refer to issue #24 Update 2.

--- Programming Exercises/Chapter 3 - Tokens, Expressions and Control Structures/Question 8/src/Answer 8.cpp
+++ Programming Exercises/Chapter 3 - Tokens, Expressions and Control Structures/Question 8/src/Answer 8.cpp
@@ -4,12 +4,42 @@
  */
 
 #include <iostream>
+#include <iomanip>
+#include <cmath>
 
 using namespace std;
 
 int main() {
 
+	cout << "Table for Y = EXP[-X]" << endl;
+
+	cout << setfill('-') << setw(129) << "-" << endl;
+
+/*
+ *  Code to display the header row properly
+ *	cout << "|" << setfill(' ') << setw(9) << "X" << flush;
+ *	cout << setw(3) << "|" << setw(10) << "0.1" << flush;
+ *	cout << setw(10) << "0.2" << setw(10) << "0.3" << setw(10) << "0.4" << flush;
+ *	cout << setw(10) << "0.5" << setw(10) << "0.6" << setw(10) << "0.7" << flush;
+ *	cout << setw(10) << "0.8" << setw(10) << "0.9" << "|" << endl;
+*/
+
+	cout << setfill('-') << setw(129) << "-" << endl;
+
+	for(float i = 0.0; i <= 9.0; i += 1.0) {
+
+		cout << "|" << setfill(' ') << setw(5) << fixed << setprecision(1) << i << setw(3) << "  |" << flush;
+
+		for(float j = 0.0; j <= 0.9; j+= 0.1)
+			cout << setw(10) << fixed << setprecision(6) << exp(-(i + j)) << "  |" << flush;
+
+		cout << endl;
+	}
+
+	cout << setfill('-') << setw(129) << "-" << endl;
+
 	return 0;
+
 }
 
 
