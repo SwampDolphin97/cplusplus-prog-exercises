ca18c3353da1011929f35b596f0b3ef142fa1d0e
Suyash Dayal <suyashdayal@gmail.com>
2017-05-30 08:41:33 +0530

    Complete 2 tasks related to answering question 3.9
    For more info, refer to issue #25 Update 2.

--- Programming Exercises/Chapter 3 - Tokens, Expressions and Control Structures/Question 9/src/Answer 9.cpp
+++ Programming Exercises/Chapter 3 - Tokens, Expressions and Control Structures/Question 9/src/Answer 9.cpp
@@ -4,10 +4,59 @@
  */
 
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+void populateData(int* dataSet, int const n);
+int findN();
+string insertSuffix(int pos);
+
 int main() {
 
+	int const n = findN();
+
+	int* dataSet = new int[n];
+
+	populateData(dataSet, n);
+
+	delete [] dataSet;
 	return 0;
+}
+
+int findN() {
+
+	int n;
+	cout << "Enter the size of the data set (N): " << flush;
+	cin >> n;
+	return n;
+}
+
+string insertSuffix(int pos) {
+
+	if(pos % 10 == 1) {
+
+		return "st";
+	}
+	else if(pos % 10 == 2) {
+		return "nd";
+	}
+	else if(pos % 10 == 3) {
+
+		return "rd";
+	}
+	else {
+
+		return "th";
+	}
+}
+
+void populateData(int* dataSet, int const n) {
+
+	cout << endl;
+	for(int i = 0; i < n; i++) {
+
+		cout << "Enter the " << i + 1 << insertSuffix(i + 1) << " value of the Data: " << flush;
+		cin >> dataSet[i];
+	}
 }
