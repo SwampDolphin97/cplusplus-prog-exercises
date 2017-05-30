00b01103588c5f95da8c69b17111ebafbedce5ae
Suyash Dayal <suyashdayal@gmail.com>
2017-05-30 16:53:00 +0530

    Solution of question 2 of chapter 4 is complete
    For more info, refer to issue #30 Update 2.

--- Programming Exercises/Chapter 4 - Functions in C++/Question 2/src/Answer 2.cpp
+++ Programming Exercises/Chapter 4 - Functions in C++/Question 2/src/Answer 2.cpp
@@ -7,6 +7,8 @@
 
 using namespace std;
 
+void readMatrix(int** matrix, int const rows, int const cols);
+void displayMatrix(int** matrix, int const rows, int const cols);
 
 int main() {
 
@@ -17,6 +19,43 @@ int main() {
 	cout << "Enter the columns of the matrix: " << flush;
 	cin >> cols;
 
+	int** matrix = new int*[rows];
+	for(int i = 0; i < rows; i++)
+		matrix[i] = new int[cols];
+
+	readMatrix(matrix, rows, cols);
+	displayMatrix(matrix, rows, cols);
+
+	for(int i = 0; i < rows; i++)
+		delete [] matrix[i];
+	delete [] matrix;
+
 	return 0;
+}
+
+void readMatrix(int** matrix, int const rows, int const cols) {
+
+	cout << endl;
+	for(int i = 0; i < rows; i++) {
+
+		for(int j = 0; j < cols; j++) {
+
+			cout << "Please enter the value for matrix[" << i + 1 << "][" << j + 1 << "]: " << flush;
+			cin >> matrix[i][j];
+		}
+		cout << endl;
+	}
+}
+
+void displayMatrix(int** matrix, int const rows, int const cols) {
+
+	cout << "Displaying values of matrix of size " << rows << " x " << cols << " ..." << endl;
+	for(int i = 0; i < rows; i++) {
+
+		for(int j = 0; j < cols; j++)
+			cout << "matrix[" << i + 1 << "][" << j + 1 << "]: " << matrix[i][j] << endl;
+
+		cout << endl;
+	}
 }
 
