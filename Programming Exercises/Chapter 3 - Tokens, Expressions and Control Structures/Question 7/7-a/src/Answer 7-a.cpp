8b28fa4ed1f7a1b78369c93813c73dbadba968b4
Suyash Dayal <suyashdayal@gmail.com>
2017-06-05 02:58:39 +0530

    Complete 3 tasks related to the answer of first part of question 3.7
    For more info, refer to issue #23 Update 3.

--- Programming Exercises/Chapter 3 - Tokens, Expressions and Control Structures/Question 7/7-a/src/Answer 7-a.cpp
+++ Programming Exercises/Chapter 3 - Tokens, Expressions and Control Structures/Question 7/7-a/src/Answer 7-a.cpp
@@ -1,13 +1,32 @@
 /*
  * Answer 7-a.cpp
- * 	Author: suyashd95
+ * Author: suyashd95
  */
 
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+inline double degToRad(double degree) {
+
+	return degree * (M_PI / 180);
+}
+
 int main() {
+
+	cout << "Evaluating the following function to 0.0001% accuracy using the series...\n" << endl;
+	cout << "sin x = x - (x^3)/3! + (x^5)/5! - (x^7)/7! + ....\n" << endl;
+
+	double degree;
+	cout << "Enter the value of x (in degrees): " << flush;
+	cin >> degree;
+
+	double const radian = degToRad(degree);
+	cout << "The angle in radians: " << radian << endl;
+
+	double const sineValue = sin(radian);
+	cout << "Its sine value: " << sineValue << endl;
 
 	return 0;
 }
