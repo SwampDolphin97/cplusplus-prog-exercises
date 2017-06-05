22f1b8e2d3d64089f53b29b8aae7301bfb07f8f2
Suyash Dayal <suyashdayal@gmail.com>
2017-06-05 09:58:09 +0530

    Complete 2 tasks listed in the TO-DO (2)
    The fundamental skeleton of the 'while(true)' loop is complete and is functioning properly.
    
    For more info, refer to issue #23 Update 5.

--- Programming Exercises/Chapter 3 - Tokens, Expressions and Control Structures/Question 7/7-a/src/Answer 7-a.cpp
+++ Programming Exercises/Chapter 3 - Tokens, Expressions and Control Structures/Question 7/7-a/src/Answer 7-a.cpp
@@ -13,7 +13,7 @@ inline double degToRad(double degree) {
 	return degree * (M_PI / 180);
 }
 
-double factorial(int nTerm);
+double factorial(int n);
 
 int main() {
 
@@ -31,22 +31,35 @@ int main() {
 	double apparentSine, term;
 	double const accuracy = 0.0001;
 
-	int nTerm = 1;
+	int power = 1;
 	bool termPositive = true;
 
+	while(true) {
 
+		// Calculate each term depending upon the exponent
+		if(power == 1)
+			term = radian;
+		else
+			term = pow(radian, power) / factorial(power);
+
+		cout << "Power: " << power << ", Term: " << term << endl;
+		if(term <= accuracy)
+			break;
+
+		power += 2;
+	}
 
 	return 0;
 }
 
 // Using Recursion to calculate the factorial of a positive number.
-double factorial(int nTerm) {
+double factorial(int n) {
 
 	int fact = 0;
-	if(nTerm == 0)
+	if(n == 0)
 		return 1;
 
-	fact = nTerm * factorial(nTerm - 1);
+	fact = n * factorial(n - 1);
 	return fact;
 }
 
