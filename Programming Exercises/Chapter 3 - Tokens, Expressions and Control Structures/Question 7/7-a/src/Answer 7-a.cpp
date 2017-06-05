1755070e5303b0fd4fdb8cfd790e96dc799e27d3
Suyash Dayal <suyashdayal@gmail.com>
2017-06-05 13:36:46 +0530

    Complete the solution to the second part of question 3.7
    For more info, refer to issue #23 Update 6.

--- Programming Exercises/Chapter 3 - Tokens, Expressions and Control Structures/Question 7/7-a/src/Answer 7-a.cpp
+++ Programming Exercises/Chapter 3 - Tokens, Expressions and Control Structures/Question 7/7-a/src/Answer 7-a.cpp
@@ -8,6 +8,8 @@
 
 using namespace std;
 
+// NOTE TO USERS: This program will work perfectly only for angles lying in the first quadrant i.e, 0 <= x <= 90.
+
 inline double degToRad(double degree) {
 
 	return degree * (M_PI / 180);
@@ -28,7 +30,7 @@ int main() {
 
 	double const trueSine = sin(radian);
 
-	double apparentSine, term;
+	double apparentSine = 0, term;
 	double const accuracy = 0.0001;
 
 	int power = 1;
@@ -46,10 +48,32 @@ int main() {
 		if(term <= accuracy)
 			break;
 
+		// Calculate apparentSine value from the alternating power series.
+		if(termPositive) {
+
+			apparentSine += term;
+			termPositive = false;
+		}
+		else {
+
+			apparentSine -= term;
+			termPositive = true;
+		}
+
 		// Increment the power by 2
 		power += 2;
 	}
 
+	cout << endl;
+	cout << "sin " << degree << " value calculated from the aforementioned series: " << apparentSine << endl;
+	cout << "sin " << degree << " value calculated from the cmath library sin x function: " << trueSine << endl;
+
+	// Testing the true sine value with the one calculated from the expansion of the power series.
+	cout << endl;
+	if(fabs(trueSine - apparentSine) <= 0.0001)
+		cout << "Result is accurate." << endl;
+	else
+		cout << "Result is not accurate." << endl;
 	return 0;
 }
 
