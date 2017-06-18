9e18504463d0b7c17475269a6ef6a5df80767313
Suyash Dayal <suyashdayal@gmail.com>
2017-06-18 18:18:52 +0530

    Design constructors of the class(es) of question 5.3 for the answer of question 6.1
    For more info, refer to issue #57 Update 3.

--- Programming Exercises/Chapter 6 - Constructors and Destructors/Question 1/src/Depositor.cpp
+++ Programming Exercises/Chapter 6 - Constructors and Destructors/Question 1/src/Depositor.cpp
@@ -26,12 +26,11 @@ void Depositor::depositAmount(double amount) {
 
 void Depositor::withdrawAmount(double amount) {
 
-	if(amount <= balance) {
+	if (amount <= balance) {
 
 		balance -= amount;
 		cout << "Transaction Successful: Rs. " << amount << " has been successfully withdrawn." << endl;
-	}
-	else {
+	} else {
 
 		cout << "Transaction Failed. Not enough balance." << endl;
 	}
@@ -42,4 +41,95 @@ void Depositor::displayInfo() {
 	cout << "Displaying Depositor's details...\n" << endl;
 	cout << "Name of the Depositor: " << name << endl;
 	cout << "Account Balance: " << balance << endl;
+}
+
+int const modclasses::Depositor::size = 10;
+
+modclasses::Depositor::Depositor() {
+
+	for(int i = 0; i < size; i++) {
+
+		name[i] = "";
+		accountNumber[i] = 0;
+		accountType[i] = 0;
+		balance[i] = 0.00;
+	}
+}
+
+void modclasses::Depositor::assignInfo() {
+
+	string name;
+	int accountNo, accountType;
+	double balance;
+
+	cout << "Entering details of the depositors...\n" << endl;
+	for(int i = 0; i < size; i++) {
+
+		cout << endl;
+		cout << "Enter the name of depositor " << i + 1 << ": " << flush;
+		cin >> name;
+		this->name[i] = name;
+
+		cout << "Enter the account number of depositor " << i + 1 << ": " << flush;
+		cin >> accountNo;
+		this->accountNumber[i] = accountNo;
+
+		cout << "Enter the account type of depositor " << i + 1 << ": 1. Saving\t2. Current\nPress the key depending on you choice:" << flush;
+		cin >> accountType;
+		accountType == 1 ? this->accountType[i] = 1 : this->accountType[i] = 2;
+
+		cout << "Enter the balance of depositor " << i + 1 << ": " << flush;
+		cin >> balance;
+		this->balance[i] = balance;
+	}
+
+	cout << "\nInfo regarding all the " << size << " depositors have been successfully entered into our database." << endl;
+}
+
+void modclasses::Depositor::depositAmount(double amount) {
+
+	cout << "\nDepositing amount..." << endl;
+	int accNum;
+	cout << "Enter the account number: " << flush;
+	cin >> accNum;
+
+	for(int i = 0; i < size; i++)
+		if(accNum == accountNumber[i])
+			balance[i] += amount;
+
+	cout << "Transaction Successful: Amount has been successfully deposited into your account." << endl;
+}
+
+void modclasses::Depositor::withdrawAmount(double amount) {
+
+	cout << "\nWithdrawing amount..." << endl;
+	int accNum;
+	cout << "Enter the account number: " << flush;
+	cin >> accNum;
+
+	for(int i = 0; i < size; i++) {
+
+		if (accNum == accountNumber[i]) {
+			if (amount <= balance[i]) {
+
+				balance[i] -= amount;
+				cout << "Transaction Successful: Rs. " << amount << " has been successfully withdrawn." << endl;
+			}
+			else {
+
+				cout << "Transaction Failed. Not enough balance." << endl;
+			}
+		}
+	}
+}
+
+void modclasses::Depositor::displayInfo() {
+
+	for(int i = 0; i < size; i++) {
+
+		cout << endl;
+		cout << "Displaying Depositor " << i + 1 << "'s details...\n" << endl;
+		cout << "Name of the Depositor: " << name[i] << endl;
+		cout << "Account Balance: " << balance[i] << endl;
+	}
 }
--- Programming Exercises/Chapter 6 - Constructors and Destructors/Question 1/src/Depositor.h
+++ Programming Exercises/Chapter 6 - Constructors and Destructors/Question 1/src/Depositor.h
@@ -15,12 +15,34 @@ private:
 	double balance;
 
 public:
-	Depositor(): name(""), accountNumber(0), accountType(0), balance(0.00) {};
-	Depositor(std::string name, int accountNo, int accountType, double balance): name(name), accountNumber(accountNo), accountType(accountType), balance(balance) {};
+	Depositor() : name(""), accountNumber(0), accountType(0), balance(0.00) {};
+	Depositor(std::string name, int accountNo, int accountType, double balance) : name(name), accountNumber(accountNo), accountType(accountType), balance(balance) {};
 	void assignInfo(std::string name, int accountNo, int accountType, double balance);
 	void depositAmount(double amount);
 	void withdrawAmount(double amount);
 	void displayInfo();
 };
 
+namespace modclasses {
+
+class Depositor {
+
+private:
+	static int const size;
+	std::string name[10];
+	int accountNumber[10];
+	int accountType[10];
+	double balance[10];
+
+public:
+	Depositor();
+	void assignInfo();
+	void depositAmount(double amount);
+	void withdrawAmount(double amount);
+	void displayInfo();
+};
+
+} /* namespace modclasses */
+
 #endif /* DEPOSITOR_H_ */
+
