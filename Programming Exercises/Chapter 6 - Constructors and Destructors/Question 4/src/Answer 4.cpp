8c1f424e121d4248de619a3aa32ca59a23a354dd
Suyash Dayal <suyashdayal@gmail.com>
2017-06-26 03:27:30 +0530

    Add the initial template of the answer to question 6.4
    For more info, refer to issue #60 Update 1.

---
+++ Programming Exercises/Chapter 6 - Constructors and Destructors/Question 4/src/Answer 4.cpp
@@ -0,0 +1,18 @@
+/*
+ * Answer 4.cpp
+ * Author: suyashd95
+ */
+
+#include <iostream>
+
+#include "Books.h"
+
+using namespace std;
+
+int main() {
+
+	Books book;
+	book.bookPurchase();
+
+	return 0;
+}
