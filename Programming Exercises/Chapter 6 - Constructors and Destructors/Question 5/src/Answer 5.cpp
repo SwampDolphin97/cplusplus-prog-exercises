837819d3c9772fa01317584af7e3c15c9434b326
Suyash Dayal <suyashdayal@gmail.com>
2017-06-26 06:34:50 +0530

    Add the initial template to the answer 6.5
    For more info, refer to issue #61 Update 1.

---
+++ Programming Exercises/Chapter 6 - Constructors and Destructors/Question 5/src/Answer 5.cpp
@@ -0,0 +1,56 @@
+/*
+ * Answer 5.cpp
+ * Author: suyashd95
+ */
+
+#include <iostream>
+
+#include "Books.h"
+
+using namespace std;
+
+int main() {
+
+	Books book;
+
+	int ch;
+	do {
+
+		cout << "1. Purchase Books\t2. Change Price\n3. Display Inventory\t4. Show Statistics\n5. Quit\nPlease enter your option: " << flush;
+		cin >> ch;
+		cout << endl;
+
+		switch(ch) {
+
+		case 1:
+			book.bookPurchase();
+			break;
+
+		case 2:
+			book.changePrice();
+			break;
+
+		case 3:
+			book.displayInventory();
+			break;
+
+		case 4:
+			Books::showStats();
+			break;
+
+		case 5:
+			cout << "Program Terminated." << endl;
+			break;
+
+		default:
+			cout << "Please select a valid option (1 to 4) " << endl;
+			break;
+		}
+
+	} while(ch != 5);
+
+	return 0;
+}
+
+
+
---
+++ Programming Exercises/Chapter 6 - Constructors and Destructors/Question 5/src/Books.h
@@ -0,0 +1,33 @@
+/*
+ * Books.h
+ * Author: suyashd95
+ */
+
+#ifndef BOOKS_H_
+#define BOOKS_H_
+
+class Books {
+
+private:
+	const char** author;
+	const char** title;
+	const char** publisher;
+	float* price;
+	int* stock;
+	static int const size = 5;
+	static int successfulTransactions;
+	static int unsuccessfulTransactions;
+
+private:
+	void populateData();
+
+public:
+	Books();
+	void bookPurchase();
+	void changePrice();
+	void displayInventory();
+	static void showStats();
+	~Books();
+};
+
+#endif /* BOOKS_H_ */
