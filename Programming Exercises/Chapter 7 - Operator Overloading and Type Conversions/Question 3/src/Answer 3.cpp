368869a97768cded8b6aba184e8f37a056f68715
Suyash Dayal <suyashdayal@gmail.com>
2017-07-18 20:09:22 +0530

    The answer to question 7.3 is complete and close issue #72

--- Programming Exercises/Chapter 7 - Operator Overloading and Type Conversions/Question 3/src/Answer 3.cpp
+++ Programming Exercises/Chapter 7 - Operator Overloading and Type Conversions/Question 3/src/Answer 3.cpp
@@ -14,50 +14,29 @@ int main() {
 	MAT m1(2, 2);
 	cout << m1 << endl;
 	cin >> m1;
-//	-m1;
-//	m1 = m1 * 5;
-//	MAT m7 = 10 * m1;
-//
+	-m1;
+	m1 = m1 * 5;
+	MAT m7 = 10 * m1;
 
 	cout << m1 << endl;
 
-//	MAT m2(3, 2);
-//	m2.populateMatrix();
-//	m2.displayMatrix();
-//	cout << endl;
-//
-//	MAT m3;
-//	m3 = m1 * m2;
-//	m3.displayMatrix();
-//	cout << endl;
+	MAT m2(3, 2);
+	cin >> m2;
+	cout << endl;
+
+	MAT m3;
+	m3 = m1 * m2;
+	cout << m3 << endl;
 
 
-//	MAT m3, m4;
-//	-m3;
-//	m3.displayMatrix();
+	MAT m8, m4;
+	-m8;
+	cout << m8 << endl;
 
-//	if(!m1)
-//		cout << "True" << endl;
-//	else
-//		cout << "False" << endl;
-
-
-//	MAT m3 = m1 + m2;
-//	m3.displayMatrix();
-//	cout << endl;
-//
-//	MAT m4;
-//	m4 = m1 + m2 + m3;
-//
-//	MAT m5;
-//	m5 = m4;
-//	m5.displayMatrix();
-//	cout << endl;
-//
-//	MAT m6;
-//	m6 = m4 - m3;
-//	m6.displayMatrix();
-//	cout << endl;
+	if(!m1)
+		cout << "True" << endl;
+	else
+		cout << "False" << endl;
 
 	return 0;
 }
