6cc6d6183e60a7622470fd0e44597f5131bd28e4
Suyash Dayal <suyashdayal@gmail.com>
2017-07-19 06:06:30 +0530

    Related to the solution to question 7.4 (Issue #73 Update 2)
    Added definitions of all the constructors (default, parameterized and copy), the copy assignment operator (=) and the destructor of the String class. (And, hence, also implemented the Rule of Three).

---
+++ Programming Exercises/Chapter 7 - Operator Overloading and Type Conversions/Question 4/src/Answer 4.cpp
@@ -0,0 +1,25 @@
+/*
+ * Answer 4.cpp
+ * Author: suyashd95
+ */
+
+#include <iostream>
+
+#include "String.h"
+
+using namespace std;
+
+int main() {
+
+	String s;
+
+	String s1 = "Hello";
+
+	String s2(s1);
+
+	String s3;
+	s3 = s2;
+}
+
+
+
--- Programming Exercises/Chapter 7 - Operator Overloading and Type Conversions/Question 4/src/String.cpp
+++ Programming Exercises/Chapter 7 - Operator Overloading and Type Conversions/Question 4/src/String.cpp
@@ -6,3 +6,44 @@
 #include "String.h"
 
 #include <cstring>
+
+String::String() {
+
+	length = 0;
+	str = new char[length + 1];
+//	std::cout << "Default constructor called." << std::endl;
+}
+
+String::String(const char* str) {
+
+	this->length = strlen(str);
+	this->str = new char[this->length + 1];
+	strcpy(this->str, str);
+//	std::cout << "Parameterized constructor called." << std::endl;
+}
+
+String::String(const String& s) {
+
+	this->length = s.length;
+	this->str = new char[this->length + 1];
+	strcpy(this->str, s.str);
+//	std::cout << "Copy Constructor called." << std::endl;
+}
+
+String& String::operator =(const String& rhs) {
+
+	this->length = rhs.length;
+	delete [] this->str;
+
+	this->str = new char[this->length + 1];
+	strcpy(this->str, rhs.str);
+//	std::cout << " Copy Assignment Operator called." << std::endl;
+
+	return *this;
+}
+
+String::~String() {
+
+	delete [] str;
+//	std::cout << "Destructor called." << std::endl;
+}
