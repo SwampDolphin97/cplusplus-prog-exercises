f015112d54c1249835832c78b8b47fb59ceb506a
Suyash Dayal <suyashdayal@gmail.com>
2017-07-29 01:01:21 +0530

    Related to the answer of question 8.2 (Issue #83 Update 2)
    Declared and defined the Account class which will act as an Abstract Base Class.

--- Programming Exercises/Chapter 8 - Inheritance: Extending Classes/Question 2/src/Account.h
+++ Programming Exercises/Chapter 8 - Inheritance: Extending Classes/Question 2/src/Account.h
@@ -6,7 +6,32 @@
 #ifndef ACCOUNT_H_
 #define ACCOUNT_H_
 
+#include <iostream>
+#include <iomanip>
+#include <string>
+
 class Account {
+
+protected:
+	std::string customerName;
+	int accountNumber;
+	std::string accountType;
+	float balance;
+
+private:
+	virtual void withdrawAmount() = 0;
+
+protected:
+	virtual void populateData() = 0;
+	virtual void displayData() const = 0;
+
+protected:
+	Account(std::string, float balance = 0.00);
+	Account(std::string, int, std::string, float);
+	~Account();
+
+public:
+	void updateBalance();
 };
 
 #endif /* ACCOUNT_H_ */
