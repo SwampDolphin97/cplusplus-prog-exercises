d45c20184b0cbc0c1d30ba0ad1d9f466eed5dc4d
Suyash Dayal <suyashdayal@gmail.com>
2017-07-31 07:10:45 +0530

    Related to the answer of question 8.3 (Issue #84 Update 8)
    1. Constructed all the blocks of the main switch case.
    2. Declared and defined all the assistant functions with default code.

--- Programming Exercises/Chapter 8 - Inheritance: Extending Classes/Question 3/src/Answer 3.cpp
+++ Programming Exercises/Chapter 8 - Inheritance: Extending Classes/Question 3/src/Answer 3.cpp
@@ -10,6 +10,11 @@
 
 using namespace std;
 
+void runTeacher(int);
+void runOfficer(int);
+void runRegular(int);
+void runCasual(int);
+
 int main() {
 
 	bool exit = false;
@@ -21,6 +26,7 @@ int main() {
 		cout << "1. Teacher\t2. Officer\n3. Regular\t4. Casual\n5. Exit" << endl;
 		cout << "Enter the number corresponding to your choice: " << flush;
 		cin >> op;
+		cout << endl;
 
 		enum {teacher = 1, officer, regular, casual, terminate};
 
@@ -28,31 +34,67 @@ int main() {
 
 		case teacher:
 		{
+			cout << setw(30) <<"<----Teacher Options---->" << endl;
+
+			cout << "\n1. Enter the details of a teacher." << endl;
+			cout << "2. Display details of a teacher." << endl;
+			cout << "3. Return to the previous menu." << endl;
+			cout << "Enter the number corresponding to your choice: " << flush;
+			int tMenu;
+			cin >> tMenu;
 			cout << endl;
-			cout << "Teacher" << endl;
+			runTeacher(tMenu);
+
 			break;
 		}
 		case officer:
 		{
+			cout << setw(30) << "<----Officer Options---->" << endl;
+
+			cout << "\n1. Enter the details of an officer." << endl;
+			cout << "2. Display details of an officer." << endl;
+			cout << "3. Return to the previous menu." << endl;
+			cout << "Enter the number corresponding to your choice: " << flush;
+			int oMenu;
+			cin >> oMenu;
 			cout << endl;
-			cout << "Officer" << endl;
+			runOfficer(oMenu);
+
 			break;
 		}
 		case regular:
 		{
+			cout << setw(30) << "<----Regular Options---->" << endl;
+
+			cout << "\n1. Enter the details of a regular." << endl;
+			cout << "2. Display details of a regular." << endl;
+			cout << "3. Return to the previous menu." << endl;
+			cout << "Enter the number corresponding to your choice: " << flush;
+			int rMenu;
+			cin >> rMenu;
 			cout << endl;
-			cout << "Regular" << endl;
+			runRegular(rMenu);
+
 			break;
 		}
 		case casual:
 		{
+			cout << setw(30) << "<----Casual Options---->" << endl;
+
+			cout << "\n1. Enter the details of a casual." << endl;
+			cout << "2. Display the details of a casual." << endl;
+			cout << "3. Return to the previous menu." << endl;
+			cout << "Enter the number corresponding to your choice: " << flush;
+			int cMenu;
+			cin >> cMenu;
 			cout << endl;
-			cout << "Casual" << endl;
+			runCasual(cMenu);
+
 			break;
 		}
 		case terminate:
 		{
-			cout << endl << "Program is terminating..." << endl;
+			cout << "Program has been successfully terminated." << endl;
 			exit = true;
 			break;
 		}
@@ -70,4 +112,22 @@ int main() {
 	return 0;
 }
 
+void runTeacher(int op) {
 
+	cout << "Running Teacher" << endl;
+}
+
+void runOfficer(int op) {
+
+	cout << "Running Officer" << endl;
+}
+
+void runRegular(int op) {
+
+	cout << "Running Regular" << endl;
+}
+
+void runCasual(int op) {
+
+	cout << "Running Casual" << endl;
+}
