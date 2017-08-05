85203e85c771f280f75efc7167db73b1ec3aaf1e
Suyash Dayal <suyashdayal@gmail.com>
2017-08-05 19:53:57 +0530

    Related to the solution to question 8.7 (Issue #88)

---
+++ Programming Exercises/Chapter 8 - Inheritance: Extending Classes/Question 7/src/GPU.h
@@ -0,0 +1,35 @@
+/*
+ * GPU.h
+ * Author: suyashd95
+ */
+
+#ifndef GPU_H_
+#define GPU_H_
+
+#include <iostream>
+#include <string>
+
+class GPU {
+
+private:
+	std::string companyName;
+	std::string modelName;
+	std::string architecture;
+	int cudaCores;
+	int baseClock;
+	int boostClock;
+	int memorySpeed;
+	std::string standardMemoryConfig;
+	int memoryBandwidth;
+	bool vrReady;
+
+protected:
+	GPU();
+	GPU(std::string, std::string, std::string, int, int, int, int, std::string, int, bool);
+	virtual ~GPU() = 0;
+
+public:
+	void display() const;
+};
+
+#endif /* GPU_H_ */
