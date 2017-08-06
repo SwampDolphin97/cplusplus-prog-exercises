39dad019b6c89f01399aa1632020eda85042cf74
Suyash Dayal <suyashdayal@gmail.com>
2017-08-06 23:00:11 +0530

    Related to the solution of question 9.1 (Issue #97 Update 2)
    Declared and defined the derived classes Rectangle and Triangle inherited from class Shape.

---
+++ Programming Exercises/Chapter 9 - Pointers, Virtual Functions and Polymorphism/Question 1/src/Rectangle.cpp
@@ -0,0 +1,14 @@
+/*
+ * Rectangle.cpp
+ * Author: suyashd95
+ */
+
+#include "Rectangle.h"
+
+#include <iostream>
+
+void Rectangle::display_area() {
+
+	std::cout << "Area of Rectangle = " << x * y << std::endl;
+}
+
---
+++ Programming Exercises/Chapter 9 - Pointers, Virtual Functions and Polymorphism/Question 1/src/Triangle.cpp
@@ -0,0 +1,13 @@
+/*
+ * Triangle.cpp
+ * Author: suyashd95
+ */
+
+#include "Triangle.h"
+
+#include <iostream>
+
+void Triangle::display_area() {
+
+	std::cout << "Area of Triangle = " << 1 / 2 * x * y << std::endl;
+}
