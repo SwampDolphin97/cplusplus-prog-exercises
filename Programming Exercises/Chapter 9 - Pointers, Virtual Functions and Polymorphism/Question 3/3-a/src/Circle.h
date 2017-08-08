12cb6e0f0a61520be3b115f29e16a9f7a0340d74
Suyash Dayal <suyashdayal@gmail.com>
2017-08-08 19:26:35 +0530

    Related to the answer of question 9.3 (Issue #99 Update 1)
    1. Created 2 sub-directories to store the answer of sub-parts (a) and (b).
    2. Copied the source code of Question 9.2 into the above mentioned sub-directories.

---
+++ Programming Exercises/Chapter 9 - Pointers, Virtual Functions and Polymorphism/Question 3/3-a/src/Circle.h
@@ -0,0 +1,17 @@
+/*
+ * Circle.h
+ * Author: suyashd95
+ */
+
+#ifndef CIRCLE_H_
+#define CIRCLE_H_
+
+#include "Shape.h"
+
+class Circle: public Shape {
+
+public:
+	void display_area();
+};
+
+#endif /* CIRCLE_H_ */
